87aba42d558742a93c1779cd9c0cbdfd5cec3fac
aleperf <aleperf@users.noreply.github.com>
2018-06-23 15:27:26 +0200

    add methods to ApisServiceModule for networking

--- app/src/main/java/com/aleperf/pathfinder/copernicana/injection/ApisServiceModule.java
+++ app/src/main/java/com/aleperf/pathfinder/copernicana/injection/ApisServiceModule.java
@@ -1,6 +1,42 @@
 package com.aleperf.pathfinder.copernicana.injection;
 
+import com.aleperf.pathfinder.copernicana.network.ApisService;
+import com.google.gson.GsonBuilder;
+
+import dagger.Provides;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ApisServiceModule {
 
     private static final String BASE_URL = "https://api.nasa.gov";
+
+    @Provides
+    @CopernicanaApplicationScope
+    public ApisService provideApisService(Retrofit retrofit) {
+        return retrofit.create(ApisService.class);
+    }
+
+    @Provides
+    @CopernicanaApplicationScope
+    public OkHttpClient provideOkHttpClient() {
+        return new OkHttpClient.Builder().build();
+    }
+
+    @Provides
+    @CopernicanaApplicationScope
+    public GsonConverterFactory provideGsonConverterFactory(){
+        return GsonConverterFactory.create(new GsonBuilder().create());
+    }
+
+    @Provides
+    @CopernicanaApplicationScope
+    public Retrofit provideRetrofit(OkHttpClient okHttpClient, GsonConverterFactory gsonConverterFactory) {
+        return new Retrofit.Builder()
+                .client(okHttpClient)
+                .baseUrl(BASE_URL)
+                .addConverterFactory(gsonConverterFactory).build();
+    }
+
 }
