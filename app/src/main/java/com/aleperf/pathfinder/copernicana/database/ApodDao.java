7fcfb46ae27bf209cf8555a0a1abc28d3b59b16c
aleperf <aleperf@users.noreply.github.com>
2018-06-23 10:28:09 +0200

    add remaining apod dao query

--- app/src/main/java/com/aleperf/pathfinder/copernicana/database/ApodDao.java
+++ app/src/main/java/com/aleperf/pathfinder/copernicana/database/ApodDao.java
@@ -2,11 +2,14 @@ package com.aleperf.pathfinder.copernicana.database;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
 
 import com.aleperf.pathfinder.copernicana.model.Apod;
 
 import java.util.List;
+
+import static android.arch.persistence.room.OnConflictStrategy.REPLACE;
 
 @Dao
 public interface ApodDao {
@@ -14,20 +17,29 @@ public interface ApodDao {
     @Query("SELECT * FROM apod ORDER BY date DESC LIMIT 1" )
     LiveData<Apod> getLastApod();
 
-    @Query("SELECT * FROM apod WHERE :date = date LIMIT 1")
+    @Query("SELECT * FROM apod WHERE date = :date LIMIT 1")
     LiveData<Apod> getApodForDate(String date);
 
     @Query("SELECT * FROM apod ORDER BY DATE DESC")
     LiveData<List<Apod>> getAllApodStartFromLast();
 
-    @Query("SELECT * FROM apod WHERE :date < date ORDER BY date DESC LIMIT 1")
+    @Query("SELECT * FROM apod WHERE date < :date ORDER BY date DESC LIMIT 1")
     LiveData<Apod> getPreviousApod(String date);
 
-    @Query("SELECT * FROM apod WHERE :date > date ORDER BY date LIMIT 1")
+    @Query("SELECT * FROM apod WHERE date > :date ORDER BY date LIMIT 1")
     LiveData<Apod> getNextApod(String date);
 
     @Query("SELECT COUNT(*) FROM apod")
     LiveData<Integer> getApodCount();
+
+    @Insert(onConflict = REPLACE)
+    void insertApod(Apod apod);
+
+    @Query("DELETE FROM apod WHERE date = :date")
+    void deleteApodWithDate(String date);
+
+    @Query("UPDATE apod SET  isFavorite = :isFavorite WHERE date = :date")
+    void updateUpod(boolean isFavorite, String date);
 
 
 
