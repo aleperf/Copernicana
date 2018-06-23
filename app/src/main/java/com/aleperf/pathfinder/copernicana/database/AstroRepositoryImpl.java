58768f02fc5a49941dd206bb032d636a3221f2e2
aleperf <aleperf@users.noreply.github.com>
2018-06-23 16:29:21 +0200

    add rx java dependencies

--- app/src/main/java/com/aleperf/pathfinder/copernicana/database/AstroRepositoryImpl.java
+++ app/src/main/java/com/aleperf/pathfinder/copernicana/database/AstroRepositoryImpl.java
@@ -3,39 +3,51 @@ package com.aleperf.pathfinder.copernicana.database;
 import android.arch.lifecycle.LiveData;
 
 import com.aleperf.pathfinder.copernicana.model.Apod;
+import com.aleperf.pathfinder.copernicana.network.ApisService;
 
 import java.util.List;
 
+import javax.inject.Inject;
+
 public class AstroRepositoryImpl implements AstroRepository {
+
+    ApodDao apodDao;
+    ApisService apisService;
+
+    @Inject
+    public AstroRepositoryImpl(ApodDao apodDao, ApisService apisService){
+        this.apodDao = apodDao;
+        this.apisService = apisService;
+    }
 
     @Override
     public LiveData<List<Apod>> getAllApodOrderDesc() {
-        return null;
+        return apodDao.getAllApodOrderDesc();
     }
 
     @Override
     public LiveData<Apod> getLastApod() {
-        return null;
+        return apodDao.getLastApod();
     }
 
     @Override
     public LiveData<Apod> getApodWithDate(String date) {
-        return null;
+        return apodDao.getApodWithDate(date);
     }
 
     @Override
     public LiveData<Apod> getPreviousApod(String date) {
-        return null;
+        return apodDao.getPreviousApod(date);
     }
 
     @Override
     public LiveData<Apod> getNextApod(String date) {
-        return null;
+        return apodDao.getNextApod(date);
     }
 
     @Override
     public LiveData<Integer> getApodCount() {
-        return null;
+        return apodDao.getApodCount();
     }
 
     @Override
