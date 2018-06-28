419dda2d89fd2a83be9fb22f5da7789f2d62df1d
aleperf <aleperf@users.noreply.github.com>
2018-06-28 17:13:03 +0200

    fix missing modules in application component

--- app/src/main/java/com/aleperf/pathfinder/copernicana/CopernicanaApplication.java
+++ app/src/main/java/com/aleperf/pathfinder/copernicana/CopernicanaApplication.java
@@ -3,10 +3,18 @@ package com.aleperf.pathfinder.copernicana;
 import android.app.Activity;
 import android.app.Application;
 
+import com.aleperf.pathfinder.copernicana.injection.ApisServiceModule;
+import com.aleperf.pathfinder.copernicana.injection.ApplicationModule;
+import com.aleperf.pathfinder.copernicana.injection.AstroDatabaseModule;
+import com.aleperf.pathfinder.copernicana.injection.CopernicanaApplicationComponent;
+import com.aleperf.pathfinder.copernicana.injection.DaggerCopernicanaApplicationComponent;
+
 
 public class CopernicanaApplication extends Application {
 
-    public static CopernicanaApplication CopernicanaApplication(Activity activity){
+    private CopernicanaApplicationComponent component;
+
+    public static CopernicanaApplication get(Activity activity){
 
         return (CopernicanaApplication) activity.getApplication();
     }
@@ -14,5 +22,16 @@ public class CopernicanaApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        component = DaggerCopernicanaApplicationComponent.builder()
+                .applicationModule(new ApplicationModule(this))
+                .astroDatabaseModule(new AstroDatabaseModule(this))
+                .apisServiceModule(new ApisServiceModule())
+                .build();
     }
+
+
+    public CopernicanaApplicationComponent getCopernicanaApplicationComponent(){
+        return component;
+    }
+
 }
